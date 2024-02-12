10d7087754a5b881352c90082bd230c0ed1170d9
marwa_madani <madanimarwa40@gmail.com>
2024-02-12 09:39:00 +0100

    Ajout du script task_2.bash

---
+++ task_2.bash
@@ -0,0 +1,10 @@
+
+echo "---------------"
+echo "Dernières 10 lignes de /etc/passwd :"
+tail /etc/passwd
+echo "---------------"
+
+echo "---------------"
+echo "Premières 10 lignes de /etc/passwd :"
+head /etc/passwd
+echo "---------------"
